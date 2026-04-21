a99431e89db4ccf616dbdee6521abfe9f4cb5656
Νίκος Φεργαδάκης <nikosfergadakis@gmail.com>
2026-04-21 21:56:23 +0200

    Resolve conflicts after stash pop

--- 5/static_sched_simulate.sh
+++ 5/static_sched_simulate.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 #BSUB -q hpc
 #BSUB -J static_parallel
-#BSUB -n 2
-#BSUB -W 00:30
-#BSUB -R "select[model==XeonGold6226R] rusage[mem=16GB]"
+#BSUB -n 32
+#BSUB -W 03:00
+#BSUB -R "rusage[mem=8GB]"
 #BSUB -R "span[hosts=1]"
-#BSUB -o static_parallel_2.out
-#BSUB -e static_parallel_2.err
-#BSUB -B
-#BSUB -N
-#BSUB -u s253129@dtu.dk
+#BSUB -o static_parallel_32.out
+#BSUB -e static_parallel_32.err
 
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate hpc
 
-time python static_sched_simulate.py 90 2
+
+time python static_sched_simulate.py 90 32
